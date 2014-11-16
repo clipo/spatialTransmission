892352beff8705e028008c3bc133bec147296db7
Carl Lipo <clipo@csulb.edu>
2014-11-16 11:21:16 -0800

    initial commit

---
+++ R/load-state-data.R
@@ -0,0 +1,17 @@
+
+setwd("/Volumes/Macintosh HD/Users/clipo/spatialTransmission/R")
+#setwd("../data/")
+file_list <- list.files("../data/namesbystate/",pattern="\\.TXT$", full.names=TRUE)
+for (file in file_list){
+  # if the merged dataset doesn't exist, create it
+  if (!exists("dataset")){
+    dataset <- read.table(file, header=FALSE, sep=",", col.names=c("State","Sex","Year","Name","Count"))
+  }
+  # if the merged dataset does exist, append to it
+  if (exists("dataset")){
+    temp_dataset <-read.table(file, header=FALSE, sep=",", col.names=c("State","Sex","Year","Name","Count"))
+    dataset<-rbind(dataset, temp_dataset)
+    rm(temp_dataset)
+  }
+  
+}
