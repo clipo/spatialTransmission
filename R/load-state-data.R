f7b4ee88105db53a1a29f0ef6a8be0df2741095a
Mark Madsen <mark@madsenlab.org>
2014-11-18 17:00:30 -0800

    Switched file loading to use the relative filepath code from package mmadsenr.
    Define an .Rprofile as described in the Slack thread, and it should load files for you.

--- R/load-state-data.R
+++ R/load-state-data.R
@@ -1,7 +1,14 @@
 
-setwd("/Volumes/Macintosh HD/Users/clipo/spatialTransmission/R")
+library(devtools)
+install_github("mmadsen/mmadsenr")
+library(mmadsenr)
+
+
+#setwd("/Volumes/Macintosh HD/Users/clipo/spatialTransmission/R")
 #setwd("../data/")
-file_list <- list.files("../data/namesbystate/",pattern="\\.TXT$", full.names=TRUE)
+#file_list <- list.files("../data/namesbystate/",pattern="\\.TXT$", full.names=TRUE)
+
+file_list <- list_files_for_data_path(directory = "spatialTransmission/data/namesbystate", pattern="\\.TXT$")
 for (file in file_list){
   # if the merged dataset doesn't exist, create it
   if (!exists("dataset")){
@@ -14,4 +21,4 @@ for (file in file_list){
     rm(temp_dataset)
   }
   
-}
+} 
