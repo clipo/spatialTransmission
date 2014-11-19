8b5755a95ccacfc87d7a41bdf71b02af3de58f36
Carl Lipo <clipo@csulb.edu>
2014-11-19 08:01:17 -0800

    now has functions to create sqlite file. sqlite database added. We use this to create other kinds of files (percents and then year by year records ala seriation)

--- python/aggregateData.py
+++ python/aggregateData.py
@@ -15,60 +15,121 @@ class aggregateData():
         self.names = {}
         self.stateNumber=0
         self.nameNumber=0
+        self.conn= None
+        self.curr= None
+        self.states=['AK','AL','AR','AZ',
+                      'CA','CO','CT','DC','DE',
+                      'FL','GA','HI','IA','ID',
+                      'IL','IN','KS','KY','LA',
+                      'MA','MD','ME','MI','MN',
+                      'MO','MS','MT','NC','ND',
+                      'NE','NH','NJ','NM','NV',
+                      'NY','OH','OK','OR','PA',
+                      'RI','SC','SD','TN','TX',
+                      'UT','VA','VT','WA','WI',
+                      'WV','WY']
         try:
             self.conn = sql.connect('babynames.sqlite')
             self.curr = self.conn.cursor()
+            self.conn.row_factory = sql.Row
         except sql.Error, e:
             print "Error %s: " % e.args[0]
             sys.exit(1)
 
+
     def openFiles(self, directory):
 
-        for file in os.listdir(directory):
-            print file
-            if file.endswith(".TXT" ) and file<>None:
-                try:
-                    logger.debug("trying to open: %s ", file)
-                    filename = open(directory+"/"+file, 'r')
-                    self.insertState(file[0:-4])
-                    self.stateNumber += 1
-                    reader = csv.reader(filename, delimiter=',', quotechar='|')
-                    for row in reader:
-                        #print row
-                        self.nameNumber += 1
-                        row = map(str, row)
-                        state = row[0]
-                        sex = row[1]
-                        year= row[2]
-                        name=row[3]
-                        count=row[4]
-                        #args =( self.nameNumber, state,year,name,sex,count,0.0)
-                        #self.curr.execute("INSERT INTO names VALUES(?,?,?,?,?,?,?)", args)
-                        #self.conn.commit
-                except csv.Error as e:
+        for state in self.states:
+            statefile=state+".TXT"
+            try:
+                logger.debug("trying to open: %s ", file)
+                filename = open(directory+"/"+statefile, 'r')
+                reader = csv.reader(filename, delimiter=',', quotechar='|')
+                self.curr.execute("DROP TABLE IF EXISTS names")
+                self.curr.execute("CREATE TABLE names (id INTEGER PRIMARY KEY Autoincrement  NOT NULL  UNIQUE , state TEXT,year INTEGER, name TEXT, sex TEXT, count INTEGER, percent NUMERIC)")
+                for row in reader:
+                    self.nameNumber += 1
+                    row = map(str, row)
+                    state = row[0]
+                    sex = row[1]
+                    year= row[2]
+                    name=row[3]
+                    count=row[4]
+                    args =( self.nameNumber, state,year,name,sex,count,0.0)
+                    #print "INSERT INTO names VALUES(?,?,?,?,?,?,?)", args
+                    self.curr.execute("INSERT INTO names VALUES(?,?,?,?,?,?,?)", args)
+                    self.conn.commit()
+            except csv.Error as e:
                     logger.error("Cannot open %s. Error: %s", file, e)
                     sys.exit('file %s does not open: %s') % ( file, e)
 
     def insertState(self, state):
-        if state <> None:
+        if self.stateNumber <> None:
             args = (self.stateNumber, state)
             print args
             self.curr = self.conn.cursor()
             self.curr.execute("Insert INTO states VALUES (?,?)",args)
-            self.conn.commit
+            self.conn.commit()
 
     def updatePercentages(self):
-        self.curr = self.conn.cursor()
-        self.curr.execute("select * from states")
-        rows = self.curr.fetchall()
-        print rows
-        for row in rows:
-            state = row[1]
+        for state in self.states:
             for y in range(1910,2013,1):
                 args=(state,y)
                 self.curr.execute("select sum(count) from names where state=? and year=?",args)
-                count=self.curr.fetchall()
-                print count
+                row=self.curr.fetchall()
+                stateyeartotal = row[0]
+                self.curr.execute("select * from names where state=? and year=?",args)
+                rows=self.curr.fetchall()
+                for row in rows:
+                    percent=row["count"]/stateyeartotal
+                    args=(percent,row["id"])
+                    self.curr.execute("update names set percent=? where id=?",args)
+
+    def createYearFiles(self):
+        for y in range(1910,2013,1):
+            ## openfile for year
+            filename=str(y)+".TXT"
+            writer=open(filename,"w")
+            ## create header
+            self.curr.execute("select name from names where year=?",y)
+            names=self.curr.fetchall()
+            with writer:
+                writer.write("State\t")
+            for n in names:
+                with writer:
+                    writer.write(names+"\t")
+            stateHash = {}
+            for state in self.states:
+
+                row=self.curr.fetchall()
+                stateyeartotal = row[0]
+                self.curr.execute("select * from names where state=? and year=?",args)
+                rows=self.curr.fetchall()
+                for row in rows:
+                    percent=row["count"]/stateyeartotal
+                    args=(percent,row["id"])
+                    self.curr.execute("update names set percent=? where id=?",args)
+
+    def test(self):
+        self.curr.execute("DROP TABLE IF EXISTS namestest")
+        self.conn.commit()
+        self.curr.execute("CREATE TABLE namestest (id INTEGER PRIMARY KEY Autoincrement  NOT NULL  UNIQUE , state TEXT,year INTEGER, name TEXT, sex TEXT, count INTEGER, percent NUMERIC)")
+        args=( (1,"CA",1910,"Bob","M",100,0.0),
+               (2,"WA",1911,"Bobby","F",200,0.0),
+               (3,"OR",1912,"Robert","M",300,0.0))
+        self.curr.executemany("INSERT INTO namestest VALUES (?,?,?,?,?,?,?)", args)
+        self.conn.commit()
+        self.curr.execute("select * from namestest")
+        rows = self.curr.fetchall()
+        rcount =0
+        for row in rows:
+            #print row
+            rcount +=1
+        if rcount <> 3:
+            logger.error("Sqlite database failure.")
+            sys.exit('problem with sqlite database. Test failed')
+
+        self.curr.execute("DROP TABLE IF EXISTS namestest")
 
     def parse_arguments(self):
         parser = argparse.ArgumentParser(description='Suck up babyname data in to sqlite database')
@@ -84,4 +145,5 @@ if __name__ == "__main__":
     babynames = aggregateData()
     args = babynames.parse_arguments()
     babynames.openFiles(directory="../data/namesbystate")
-    babynames.updatePercentages()
+    #babynames.updatePercentages()
+    #babynames.test()
